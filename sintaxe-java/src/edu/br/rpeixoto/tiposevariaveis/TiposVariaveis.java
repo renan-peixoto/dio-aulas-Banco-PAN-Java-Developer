5bba5d2176f7a1056e124f6aacc0986fd70b51b2
renan-peixoto <renan2.peixoto@gmail.com>
2023-02-08 16:57:05 -0300

    Tipos e variaveis - tipos primitivos - variaveis - constantes - classe String

--- sintaxe-java/src/edu/br/rpeixoto/tiposevariaveis/TiposVariaveis.java
+++ sintaxe-java/src/edu/br/rpeixoto/tiposevariaveis/TiposVariaveis.java
@@ -1,7 +1,42 @@
 package edu.br.rpeixoto.tiposevariaveis;
 
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    /**
+     * Tipos primitivos, variaveis e constantes
+     * 
+     */
+    public static void main(String[] args) {
+        // VARIAVEIS
+        System.out.println("**** VARIAVEIS ****");
+
+        byte idade = 123;
+        System.out.println(idade);
+        idade = 121; // variaveis podem mudar de valor
+        System.out.println(idade);
+
+        short ano = 2021;
+        System.out.println(ano);
+
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        System.out.println(cep);
+
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        System.out.println(cpf);
+
+        double salario = 1275.33;
+        System.out.println(salario);
+
+        // CONSTANTES - CAIXA ALTA e com a palavra reservada final
+        System.out.println("\n **** CONSTANTES ****");
+
+        final float VALOR_DE_PI = 3.14F;
+        System.out.println(VALOR_DE_PI);
+
+        // Não é um tipo primitivo e sim uma classe utilizada para textos.
+        System.out.println("\n **** CLASSE STRING ****");
+
+        String meuNome = "Renan";
+        System.out.println(meuNome);
+
     }
 }
